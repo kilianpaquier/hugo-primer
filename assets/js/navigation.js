d820ac2f3f4244ccf676898410a38f53bbace90a
kilianpaquier <kilian@kilianpaquier.com>
2025-05-02 19:53:02 +0000

    fix(nav): compute more precisely navigation items width to only show overlay menu when really necessary
    Signed-off-by: kilianpaquier <kilian@kilianpaquier.com>

--- assets/js/navigation.js
+++ assets/js/navigation.js
@@ -1,15 +1,50 @@
 import wait from "./lib/wait"
 
+/**
+ * width measures the width of an HTML element.
+ * 
+ * @param {HTMLElement} el the element to measure its width
+ * 
+ * @returns {number} the input element width
+ */
+const getWidth = el => {
+    const originalStyle = el.getAttribute("style") || "";
+    const none = el.classList.contains("d-none")
+
+    el.style.position = 'absolute';
+    el.style.visibility = 'hidden';
+    el.style.display = 'block';
+    el.style.left = '-9999px';
+    if (none) el.classList.remove("d-none")
+
+    const width = el.offsetWidth
+
+    if (none) el.classList.add("d-none")
+    el.setAttribute('style', originalStyle)
+
+    return width
+}
+
 // wait for underline-nav to be initialized within 3000 milliseconds
 wait("#underline-nav", 3000).then(nav => {
+    /** @type {NodeList} */
     const items = nav.querySelector("ul").querySelectorAll("li")
-    const button = document.getElementById("menu-button")
-    const overlay = document.getElementById("menu-overlay")
+
+    /** @type {HTMLButtonElement} */
+    const button = nav.getElementById("menu-button")
+
+    /** @type {HTMLElement} */
+    const overlay = nav.getElementById("menu-overlay")
+
+    // measure the whole navigation width and compute its mean
+    let width = getWidth(button)
+    items.forEach(el => width += getWidth(el))
+    width /= items.length
 
     new ResizeObserver(() => {
         // Note: maybe fixed value 155, base width for a navigation element is invalid or not precise enough
         // can evolve in the future
-        const showing = Math.floor(nav.clientWidth / 155)
+        const showing = Math.floor(nav.clientWidth / width)
 
         // display (or not) the menu button overlay
         if (showing < items.length) {
